9385be4162662d18db9f92f96486c7f12bf98edc
Douglas Santos <foxgoldseller@gmail.com>
2024-01-05 16:03:28 -0300

    fix update codigo para ficar mais simples

--- app.js
+++ app.js
@@ -1,133 +1,114 @@
-const filterContainer = document.querySelector(".filter-container");
-const charactersCountElement = document.querySelector(
-  "[data-count-characters]"
-);
-const locationsCountElement = document.querySelector("[data-count-locations]");
-const episodesCountElement = document.querySelector("[data-count-episodes]");
-
-const charactersURL = "https://rickandmortyapi.com/api/character/?page=1";
+const charactersURL = "https://rickandmortyapi.com/api/character";
 const episodesURL = "https://rickandmortyapi.com/api/episode";
 const locationsURL = "https://rickandmortyapi.com/api/location";
 
-async function apiDataLoader(page = 1) {
+let currentpage = 1;
+async function apiDataLoad() {
   const characters = await axios.get(`${charactersURL}`);
-  const episodes = await axios.get(`${episodesURL}/?page=${page}`);
-  const locations = await axios.get(`${locationsURL}/?page=${page}`);
-
+  const episodes = await axios.get(`${episodesURL}`);
+  const locations = await axios.get(`${locationsURL}`);
   return {
     characters: characters.data,
     episodes: episodes.data,
     locations: locations.data,
   };
 }
+async function apiDataLoadEpisode(url) {
+  const episodes = await axios.get(url);
 
-filter.addEventListener("focus", () => {
-  filterContainer.classList.add("focused");
-});
-
-filter.addEventListener("blur", () => {
-  filterContainer.classList.remove("focused");
-});
-
-function mountCard(image, name, status, species, location, episode) {
-  return `
-  <article class="card">
-  <img class="character-image" src="${image}" alt="Character image">
-  <div class="character-info">
-      <div>
-          <h2>${name}</h2>
-          <h3><span class="status ${status}"></span>${translateStatus(
-    status
-  )} - ${translateSpeciesName(species)}</h3>
-      </div>
-      <div>
-          <p>Última localização conhecida:</p>
-          <h3>${location.name}</h3>
-      </div>
-      <div>
-          <p>Visto a última vez em:</p>
-          <h3>${episode}</h3>
-      </div>
-  </div>
-</article>
-  `;
+  return {
+    episodes: episodes.data,
+  };
 }
-
-async function fetchLastSeenEpisode(episodes) {
-  return await axios.get(episodes[episodes.length - 1]);
+async function getEpisode(url) {
+  const res = await apiDataLoadEpisode(url);
+  return res.episodes.name;
 }
-
-async function fetchCharactersByPage(url) {
-  try {
-    const response = await axios.get(url);
-    const characters = response.data.results;
-
-    changePageContextData(
-      response.data.info.pages,
-      response.data.info.prev,
-      response.data.info.next
-    );
-    changePagesToShow();
-    addNumberPages();
-
-    container.innerHTML = "";
-    characters.forEach(
-      async ({ name, status, location, image, episode, species }) => {
-        const episodeName = (await fetchLastSeenEpisode(episode)).data.name;
-        container.innerHTML += mountCard(
-          image,
-          name,
-          status,
-          species,
-          location,
-          episodeName
-        );
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    alert("Não foi possível buscar personagens");
-  }
-}
-fetchCharactersByPage(charactersURL);
-
-async function getCharactersByName(name) {
-  return await axios.get(
-    `https://rickandmortyapi.com/api/character/?name=${name}`
-  );
-}
-
-filter.addEventListener("keyup", async (e) => {
-  container.innerHTML = '<div class="loader"></div>';
-  const filteredCharacters = (await getCharactersByName(e.target.value)).data
-    .results;
-  // TODO: erro se não achou nome
-  setTimeout(() => {
-    container.innerHTML = "";
-    filteredCharacters.forEach(
-      async ({ name, status, location, image, episode, species }) => {
-        const episodeName = (await fetchLastSeenEpisode(episode)).data.name;
-        container.innerHTML += mountCard(
-          image,
-          name,
-          status,
-          species,
-          location,
-          episodeName
-        );
-      }
-    );
-  }, 300);
-});
 
 async function getDataCount(data) {
-  const res = await apiDataLoader();
+  const res = await apiDataLoad();
   return res[data].info.count;
 }
-
-async function printApiEnpointsInfoAmount() {
-  charactersCountElement.textContent = await getDataCount("characters");
-  locationsCountElement.textContent = await getDataCount("locations");
-  episodesCountElement.textContent = await getDataCount("episodes");
+async function mostrarCount() {
+  const characters = await getDataCount("characters");
+  const episodes = await getDataCount("episodes");
+  const locations = await getDataCount("locations");
+  personagens.innerHTML = characters;
+  localizacoes.innerHTML = locations;
+  episodios.innerHTML = episodes;
 }
-printApiEnpointsInfoAmount();
+mostrarCount();
+
+async function apiDataLoadCards(name, currentpage) {
+  const characters = await axios.get(
+    `${charactersURL}/?name=${name}&page=${currentpage}`
+  );
+  return characters.data.results;
+}
+
+
+async function montarCard() {
+  console.log("currentPage ", currentpage);
+
+  const searchTerm = document.getElementById("buscador").value;
+  pages.innerHTML = currentpage;
+  const cards = await apiDataLoadCards(searchTerm, currentpage);
+  container.innerHTML = "";
+  let index = 0;
+
+  for (const personagem of cards) {
+    index++;
+    console.log("primeiro index ", index);
+    const episodeName = await getEpisode(personagem.episode[personagem.episode.length - 1]);
+
+    container.innerHTML += `
+      <article class="card">
+        <img class="character-image" src="${personagem.image}" alt="Character image">
+        <div class="character-info">
+          <div>
+            <h2>${personagem.name}</h2>
+            <h3>${status(personagem)} - ${personagem.species}</h3>
+          </div>
+          <div>
+            <p>Última localização conhecida:</p>
+            <h3>${personagem.location.name}</h3>
+          </div>
+          <div>
+            <p>Visto a última vez em:</p>
+            <h3>${episodeName}</h3>
+          </div>
+        </div>
+      </article>`;
+
+    // Add horizontal line after every two cards
+    if (index % 2 === 0 && index !== cards.length) {
+      container.innerHTML += `<div class="horizontal-line"></div>`;
+    }
+  }
+}
+  
+  montarCard()
+
+  function status(cards) {
+    if (cards.status == "Alive") {
+      return `<h4 >💚 Vivo`;
+    }
+    if (cards.status == "Dead") {
+      return `<h4 >❤️ Morto`;
+    } else {
+      return `<h4 >🩶 Desconhecido`;
+    }
+  }
+  function proxima() {
+    currentpage++
+    montarCard()
+  } 
+   function anterior() {
+    currentpage--
+    montarCard()
+  }
+
+  function buscar() {
+    currentpage =1
+    montarCard()
+  }
--- page-arrows.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.back-page-button').addEventListener('mouseover', () => {
-  document.querySelector('.ph-arrow-fat-left').classList.add('move-left-arrow');
-});
-document.querySelector('.back-page-button').addEventListener('mouseout', () => {
-  document.querySelector('.ph-arrow-fat-left').classList.remove('move-left-arrow');
-});
